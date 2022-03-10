b7ce9ee6de4112264d448b69843aae7cac7228b4
Damilola Alao <oluwadamilola.alao@gmail.com>
2022-03-10 03:12:12 +0100

    first commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x11720e9bFbb17574a1A511d3aE552F1D96a355DE";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
